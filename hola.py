32b826e17f30f0f48a8a8fe9bf9275782e494501
valeriaroman16 <a01662483@tec.mx>
2024-04-17 19:44:59 -0600

    requirements

--- hola.py
+++ hola.py
@@ -1,6 +1,7 @@
 #importamos la biblioteca streamlit
 import pandas as pd
 import streamlit as st
+import matplotlib as plt
 
 #creamos el título de la app
 st.title("Mi primera App")
@@ -10,3 +11,8 @@ st.write("Graficas del dataset")
 titanic_link = 'titanic.csv'
 titanic_data =pd.read_csv(titanic_link)
 st.dataframe(titanic_data)
+
+fig, ax = plt.subplots()
+ax.hist(titanic_data["Fare"])
+st.header("Histograma del Titanic")
+st.pyplot(fig)
